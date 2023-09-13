7408bafdafa9737220984b4f8b3e2a967d25f711
newtonr12 <newtonron12@gmail.com>
2023-09-13 13:33:32 -0500

    code revision

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-
-//this prints out symbol on the calculator screen..
-function printSymbol(e)
-{
-    let result = document.getElementById("result");
-    let obj = e.target;
-    
-    result.value = result.value + obj.textContent;
-}
 //if the clicked object is a digit 
-function printDigit(e)
+function printSymbol(e)
 {
     let result = document.getElementById("result");
     let obj = e.target;
@@ -18,26 +9,12 @@ function printDigit(e)
     else
         result.value = result.value + obj.textContent;
 }
-
-
-num0.addEventListener("click", printDigit);
-num1.addEventListener("click", printDigit);
-num2.addEventListener("click", printDigit);
-num3.addEventListener("click", printDigit);
-num4.addEventListener("click", printDigit);
-num5.addEventListener("click", printDigit);
-num6.addEventListener("click", printDigit);
-num7.addEventListener("click", printDigit);
-num8.addEventListener("click", printDigit);
-num9.addEventListener("click", printDigit);
-sub.addEventListener("click", printSymbol);
-plus.addEventListener("click", printSymbol);
-mult.addEventListener("click", printSymbol);
-div.addEventListener("click", printSymbol);
-decimal.addEventListener("click", printSymbol);
-leftp.addEventListener("click", printSymbol);
-rightp.addEventListener("click", printSymbol);
-clear.addEventListener("click", 
-    () => document.getElementById("result").value = 0 );
-//clear.addEventListener("click", clearScreen);
+ 
+let result = document.getElementById("result");
+let clear = document.getElementById("clear");
+let equal = document.getElementById("equal");
+let symbols = document.getElementsByClassName("symbol")
+for (let i = 0; i < symbols.length; i++)
+    symbols[i].addEventListener("click", printSymbol);
+clear.addEventListener("click", () => result.value = 0 );
 equal.addEventListener("click", e => result.value = eval(result.value) )
