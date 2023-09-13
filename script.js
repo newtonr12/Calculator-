2049ca0944dc7933f43c37a16befe1d799cf9721
newtonr12 <newtonron12@gmail.com>
2023-09-13 16:00:19 -0500

    modified code

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-//if the clicked object is a digit 
-function printSymbol(e)
-{
+function printSymbol(e) {
     let result = document.getElementById("result");
     let obj = e.target;
-    
-    if (result.value == 0)
+
+    if (obj.id === "ce-ac") {
+        if (result.value !== "0") {
+            result.value = "0"; // AC functionality
+        } else {
+            let currentValue = result.value;
+            result.value = currentValue.slice(0, -1); // CE functionality
+        }
+    } else if (result.value == "0") {
         result.value = obj.textContent;
-    else
+    } else {
         result.value = result.value + obj.textContent;
+    }
 }
- 
+
+let ceAcButton = document.getElementById("ce-ac"); // Modify the button ID
 let result = document.getElementById("result");
-let clear = document.getElementById("clear");
-let equal = document.getElementById("equal");
-let symbols = document.getElementsByClassName("symbol")
-for (let i = 0; i < symbols.length; i++)
+let symbols = document.getElementsByClassName("symbol");
+
+for (let i = 0; i < symbols.length; i++) {
     symbols[i].addEventListener("click", printSymbol);
-clear.addEventListener("click", () => result.value = 0 );
-equal.addEventListener("click", e => result.value = eval(result.value) )
+}
+
+ceAcButton.addEventListener("click", printSymbol); // Listen for clicks on the CE/AC button
+let equal = document.getElementById("equal");
+
+equal.addEventListener("click", () => {
+    try {
+        result.value = eval(result.value);
+    } catch (error) {
+        result.value = "Error"; // Display an error message
+    }
+});
